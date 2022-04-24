ba86cb19736ebaa147b9d4d40f42ad75dc88188c
Manuel Lerchner <manuel.lerchner1111@gmail.com>
2022-04-24 19:38:41 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="container">
+        <Header title="Task Tracker" />
+        <Tasks @delete-task="deleteTask" :tasks="tasks" />
+    </div>
+</template>
+
+<script lang="ts">
+import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
+
+export default {
+    name: "App",
+
+    components: {
+        Header,
+        Tasks,
+    },
+    data() {
+        return {
+            tasks: [],
+        };
+    },
+    methods: {
+        deleteTask(id: number) {
+            this.tasks = this.tasks.filter((task) => task.id !== id);
+        },
+    },
+    created() {
+        this.tasks = [
+            {
+                id: 1,
+                title: "Learn Vue",
+                day: "Monday",
+                reminder: true,
+            },
+            {
+                id: 2,
+                title: "Learn Vuex",
+                day: "Tuesday",
+                reminder: false,
+            },
+            {
+                id: 3,
+                title: "Learn Vue Router",
+                day: "Wednesday",
+                reminder: false,
+            },
+        ];
+    },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+body {
+    font-family: "Poppins", sans-serif;
+}
+.container {
+    max-width: 500px;
+    margin: 30px auto;
+    overflow: auto;
+    min-height: 300px;
+    border: 1px solid steelblue;
+    padding: 30px;
+    border-radius: 5px;
+}
+.btn {
+    display: inline-block;
+    background: #000;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    margin: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    font-size: 15px;
+    font-family: inherit;
+}
+.btn:focus {
+    outline: none;
+}
+.btn:active {
+    transform: scale(0.98);
+}
+.btn-block {
+    display: block;
+    width: 100%;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="onClick()" :style="{ background: color }" class="btn">{{ text }}</button>
+</template>
+
+<script lang="ts">
+export default {
+    name: "Button",
+    props: {
+        text: {
+            type: String,
+            required: true,
+        },
+        color: {
+            type: String,
+            required: false,
+            default: "black",
+        },
+    },
+    methods: {
+        onClick() {
+         console.log("Button clicked!");
+        },
+    },
+};
+</script>
